3e7c01db4ee748b4a3231cf83d785e883fca192a
Sam Groveman <samgroveman@outlook.com>
2025-08-02 09:06:22 -0400

    Cleaned up shell script

--- drive_temp_hook.sh
+++ drive_temp_hook.sh
@@ -13,26 +13,28 @@ fi
 # Full path to 'smartctl' program:
 smartctl=$(which smartctl)
 
+# Choose one of the following two methods to specify the drives you want to monitor:
+
 # 1. A string constant; just key in the devices you want to report on here:
 drives="/dev/sda /dev/sdb /dev/sdc"
 
-
 # 2. A smartctl-based function:
 
-# get_smart_drives()
-# {
-#   gs_smartdrives=""
-#   gs_drives=$("${smartctl}" --scan | awk '{print $1}')
+get_smart_drives()
+{
+  gs_smartdrives=""
+  gs_drives=$("${smartctl}" --scan | awk '{print $1}')
 
-#   for gs_drive in ${gs_drives}; do
-#     gs_smart_flag=$("${smartctl}" -i "${gs_drive}" | egrep "SMART support is:[[:blank:]]+Enabled" | awk '{print $4}')
-#     if [ "$gs_smart_flag" = "Enabled" ]; then
-#       gs_smartdrives="${gs_smartdrives} ${gs_drive}"
-#     fi
-#   done
-#   #echo "${gs_smartdrives}"
-# }
+  for gs_drive in ${gs_drives}; do
+    gs_smart_flag=$("${smartctl}" -i "${gs_drive}" | egrep "SMART support is:[[:blank:]]+Enabled" | awk '{print $4}')
+    if [ "$gs_smart_flag" = "Enabled" ]; then
+      gs_smartdrives="${gs_smartdrives} ${gs_drive}"
+    fi
+  done
+  #echo "${gs_smartdrives}"
+}
 
+# Uncomment the next line to use the function to get the drives:
 #drives=$(get_smart_drives)
 
 
@@ -44,10 +46,7 @@ drives="/dev/sda /dev/sdb /dev/sdc"
 while :
 do
   hottest=0
-
   for drive in ${drives}; do
-    #serial=$("${smartctl}" -i "${drive}" | grep -i "serial number" | awk '{print $NF}')
-    #capacity=$("${smartctl}" -i "${drive}" | grep "User Capacity" | awk '{print $5 $6}')
     temp=$("${smartctl}" -A "${drive}" | grep "194 Temperature" | awk '{print $10}')
     if [ -z "${temp}" ]; then
       temp=$("${smartctl}" -A "${drive}" | grep "190 Temperature_Case" | awk '{print $10}')
@@ -61,8 +60,8 @@ do
     if [ -z "${temp}" ]; then
       temp="0"
     fi
-    # echo "$temp"
-    # if [ $(("temp")) -gt "$hottest" ]; then
+    # echo "$drive": "$temp"
+    # if [ $(("temp")) -gt "$hottest" ]; then # Alternative POSIX-like syntax
     if (($(("temp")) > "$hottest")); then
       hottest=$(("temp"))
     fi
